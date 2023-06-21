a21bc209152de947876cc803f17fad8422bf1065
sumitdas <sumit260799@gmail.com>
2023-06-21 13:47:52 +0530

    final

--- db/routes.js
+++ db/routes.js
@@ -21,6 +21,75 @@ router.get("/", async (req, res) => {
     res.status(400).send(error);
   }
 });
+router.get("/api/get", (req, res) => {
+  const searchTerm = req.query.searchTerm;
+
+  // Use Mongoose to perform the search
+
+  User.find({ fname: { $regex: searchTerm, $options: "i" } })
+    .then((results) => res.json(results))
+    .catch((error) =>
+      res.status(500).json({ error: "An error occurred while searching" })
+    );
+});
+// findOne...........
+// router.get("/get/:id", async (req, res) => {
+//   try {
+//     // Find the item with the specified id
+//     const user = await User.find({ _id: request.params.id });
+//     response.status(200).json(user);
+//   } catch (error) {
+//     res.status(500).send("Server error");
+//   }
+// });
+
+router.get("/get/:id", (req, res) => {
+  const id = req.params.id; // Get the id parameter from the URL
+  // Use findById method in the model
+  User.findById(id)
+    .then((doc) => {
+      if (!doc) {
+        // Handle the case when the document is not found
+        console.log("Document not found");
+        return;
+      }
+
+      // Document found, do something with it
+      res.send(doc);
+    })
+    .catch((err) => {
+      // Handle the error
+      console.error(err);
+    });
+});
+//update method
+// router.patch("/:id", async (req, res) => {
+//   const id = req.params.id;
+//   // const user = req.body;
+//   // const editUser = new User(user)
+//   const newData = new User(req.body);
+//   try {
+//     // await newData.save();
+//     await User.updateOne({ id }, newData);
+//     res.status(201).json(newData);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).send(error);
+//   }
+// });
+
+router.patch("/:id", async (req, res) => {
+  let user = req.body;
+
+  const editUser = new User(user);
+  try {
+    await User.updateOne({ _id: req.params.id }, editUser);
+    res.status(201).json(editUser);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+});
+//
 router.delete("/delete/:id", (req, res) => {
   const { id } = req.params;
   User.findByIdAndDelete(id)
